febcfea20bc61fe90944dc2c882168f41f581e4d
Marcos Ledesma <marcos.ledesma.picazo@cieep.com>
2020-12-11 18:58:38 +0100

    Ejs0102

--- src/Ejercicio01/Animal.java
+++ src/Ejercicio01/Animal.java
@@ -7,31 +7,27 @@ public class Animal extends Thread {
     // que se ejecuten concurrentemente y utiliza la asignación de prioridades para
     // establecer quién llegará el primero. La carrera será mediante un contador de
     // iteraciones y ganará el primero que llegue a 10000 iteraciones.
-    private int contador;
-    public static int ganador = 0;
+
+    // Para saber en qué posición termina
+    public static int posicionFinal = 1;
 
     // Constructor con nombre
-    public Animal(String name, int prioridad, int contador) {
+    public Animal(String name, int prioridad) {
         super(name);
         this.setPriority(prioridad);
-        this.contador = contador;
     }
 
     // Método run
     @Override
     public void run() {
         //super.run();
-        System.out.println("\nEntrando en la carrera " + this.getName());   // Hilo que está entrando
+        // Hilo que está entrando
+        System.out.println("\nEntrando en la carrera " + this.getName());
 
-        for (contador = 0; contador < 10; contador++) {
-            System.out.println(this.getName() + " lleva " + contador);
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(this.getName() + " lleva " + i);
         }
-        ganador++;
-
-
-        if (this.ganador == 1) {
-            this.interrupt();   // No interrumpe al resto (siguen en la carrera)
-            System.out.println("\nGANADOR -> " + this.getName() + "\n");
-        }
+        // Fin de la carrera
+        System.out.println("\n"+this.getName() + " ha terminado " + posicionFinal++ + "\n");
     }
 }
--- src/Ejercicio01/Carrera.java
+++ src/Ejercicio01/Carrera.java
@@ -3,13 +3,23 @@ package Ejercicio01;
 public class Carrera {
     public static void main(String[] args) {
         // Hilos
-        Animal tortuga = new Animal("Tortuga",1, 0);
-        Animal liebre = new Animal("Liebre",2, 0);
-        Animal guepardo = new Animal("Guepardo",3, 0);
+        Animal tortuga = new Animal("Tortuga",1);
+        Animal liebre = new Animal("Liebre",5);
+        Animal guepardo = new Animal("Guepardo",10);
 
         // Comenzar hilos
         tortuga.start();
         liebre.start();
         guepardo.start();
+
+        // Esperar a que un hilo termine para que el hilo principal avise
+        try {
+            guepardo.join();
+            tortuga.join();
+            liebre.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Soy el hilo principal y el resto han terminado");
     }
 }
